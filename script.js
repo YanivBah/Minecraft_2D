f78b322af7847aa2f407f60ee489f80159a246ca
Yaniv Bahalker <ybahalker@gmail.com>
2021-02-25 11:18:15 +0200

    Random Clouds Generation

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const database = {
   var: {
     currentTool: '',
+    currentBlock: '',
 
   },
 
@@ -24,6 +25,9 @@ const database = {
 
   functions: {
     generateWorld: (e) => {
+      if(document.querySelectorAll('.block')) {
+        document.querySelectorAll('.block').forEach(e => e.remove());
+      }
       const height = database.settings.height || 40;
       const width = database.settings.width || 40;
       const div = document.createElement('div');
@@ -40,18 +44,46 @@ const database = {
       }
     },
 
+    generateSky: (e) => {
+      let number = database.functions.randomNumber(16,30);
+      for (let row = 1;row <= number;row++) {
+        const current = document.querySelectorAll(`.block[row="${row}"`);
+        current.forEach(el => el.setAttribute('data-block-type', 'sky'));
+      }
+    },
+
+    generateCloud: (height = 4) => {
+      const skyBlocks = document.querySelectorAll('.block[data-block-type="sky"]');
+      let number = skyBlocks[skyBlocks.length - 1].getAttribute('row');
+      const startRow = parseInt(number/2.6);
+      number = database.functions.randomNumber(2,26);
+      let number2 = 10;
+      for (let row = startRow; row > startRow-height; row--) {
+        for (let col = number; col < number+number2; col++) {
+          const current = document.querySelector(`.block[col="${col}"][row="${row}"]`);
+          current.setAttribute('data-block-type', 'cloud');
+        }
+        number2-=2;
+        number++;
+      }
+    },
+
     mineable: (e) => {
-      // Get what the clicked block type is
-      const blockType = e.target.getAttribute('data-block-type');
-      const currentTool = database.var.currentTool;
-      // Check that the block mineable by the current tool
-      const ableToMine = database.blocks[currentTool].indexOf(blockType);
       if (currentTool) {
+        // Get what the clicked block type is
+        const blockType = e.target.getAttribute('data-block-type');
+        const currentTool = database.var.currentTool;
+        // Check that the block mineable by the current tool
+        const ableToMine = database.blocks[currentTool].indexOf(blockType);
         return ableToMine >= 0 ? console.warn('Mineable:',true) : console.warn('Mineable:',false);
       }
     },
 
-    placeable: (e) => {},
+    placeable: (e) => {
+      if (currentBlock && e.target.getAttribute('data-block-type') === 'sky') {
+
+      }
+    },
 
     pickTool: (e) => {
       const index = [...e.target.parentElement.children].indexOf(e.target);
@@ -59,15 +91,34 @@ const database = {
       console.warn('currentTool = ' + database.var.currentTool);
     },
 
-    pickBlock: (e) => {},
+    pickBlock: (e) => {
+      // remove Current Tool selected
+      database.var.currentTool = '';
+      // add Current Block
+      database.var.currentBlock = '';
+    },
 
     updateInventory: (e) => {},
+
+    randomNumber: (min,max) => parseInt(Math.random() * (max - min) + min),
   }
 };
 
 // Start Menu Gone
 const startButton = document.querySelector('.btn-1');
-startButton.addEventListener('click', database.functions.generateWorld);
+startButton.addEventListener('click', el => {
+  database.functions.generateWorld(el);
+  database.functions.generateSky(el);
+  database.functions.generateCloud(database.functions.randomNumber(3,5));
+});
+
+const start = () => {
+  database.functions.generateWorld();
+  database.functions.generateSky();
+  database.functions.generateCloud(database.functions.randomNumber(3,5));
+  database.functions.generateCloud(database.functions.randomNumber(4,7));
+  database.functions.generateCloud(database.functions.randomNumber(3,6));
+}
 
 // Toolbar
 const tools = document.querySelectorAll('.tool');
