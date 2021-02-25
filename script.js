a0643597bb2330347a272cd7614aca64290eecce
Yaniv Bahalker <ybahalker@gmail.com>
2021-02-25 18:18:33 +0200

    mining block, randomize dirt,clouds,sky

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ const database = {
     generateCloud: (height = 4) => {
       const skyBlocks = document.querySelectorAll('.block[data-block-type="sky"]');
       let number = skyBlocks[skyBlocks.length - 1].getAttribute('row');
-      const startRow = parseInt(number/2.6);
+      let startRow = parseInt(number/4);
       number = database.functions.randomNumber(2,26);
       let number2 = 10;
       for (let row = startRow; row > startRow-height; row--) {
@@ -66,16 +66,65 @@ const database = {
         number2-=2;
         number++;
       }
+      // number = skyBlocks[skyBlocks.length - 1].getAttribute('row');
+      // startRow = parseInt(number/3);
+      // number = database.functions.randomNumber(2,26);
+      // for (let row = startRow; row > startRow-height; row--) {
+      //   for (let col = number; col < number+number2; col++) {
+      //     const current = document.querySelector(`.block[col="${col}"][row="${row}"]`);
+      //     current.setAttribute('data-block-type', 'cloud');
+      //   }
+      //   number2-=2;
+      //   number++;
+      // }
+    },
+
+    generateGrass: () => {
+      const skyBlocks = document.querySelectorAll('.block[data-block-type="sky"]');
+      let startRow = skyBlocks[skyBlocks.length - 1].getAttribute('row');
+      let randomize = [database.functions.randomNumber(2,39),database.functions.randomNumber(2,39),database.functions.randomNumber(2,39),database.functions.randomNumber(2,39)];
+      for (let col = 1; col <= 40;col++) {
+        const current = document.querySelector(`.block[col="${col}"][row="${startRow}"]`);
+        current.setAttribute('data-block-type','grass');
+        col === randomize[0] ? startRow -=2 : '';
+        col === randomize[1] ? startRow++ : '';
+        col === randomize[2] ? startRow-- : '';
+      }
+    },
+
+    generateDirt: () => {
+      const grassBlock = document.querySelectorAll('.block[data-block-type="grass"]');
+      for (let count = 1; count <= 6; count++) {
+        for (let index = 0; index < grassBlock.length ;index++) {
+          const currentRow = Number(grassBlock[index].getAttribute('row'));
+          const currentCol = grassBlock[index].getAttribute('col');
+          const dirtBlock = document.querySelector(`.block[col="${currentCol}"][row="${currentRow+count}"]`);
+          dirtBlock.setAttribute('data-block-type','dirt');
+        }
+      }
+    },
+
+    listeners: () => {
+      const blocks = document.querySelectorAll('.block');
+      blocks.forEach(e => e.addEventListener('click', database.functions.mineBlock));
     },
 
     mineable: (e) => {
-      if (currentTool) {
+      if (database.var.currentTool) {
         // Get what the clicked block type is
         const blockType = e.target.getAttribute('data-block-type');
         const currentTool = database.var.currentTool;
         // Check that the block mineable by the current tool
         const ableToMine = database.blocks[currentTool].indexOf(blockType);
-        return ableToMine >= 0 ? console.warn('Mineable:',true) : console.warn('Mineable:',false);
+        console.log('Test');
+        return ableToMine >= 0 ? true : false;
+      }
+    },
+
+    mineBlock: (e) => {
+      if (database.functions.mineable(e)) {
+        const current = e.target;
+        current.setAttribute('data-block-type','sky');
       }
     },
 
@@ -101,6 +150,7 @@ const database = {
     updateInventory: (e) => {},
 
     randomNumber: (min,max) => parseInt(Math.random() * (max - min) + min),
+    randomFloatNumber: (min,max) => (Math.random() * (max - min) + min).toFixed(2),
   }
 };
 
@@ -109,18 +159,26 @@ const startButton = document.querySelector('.btn-1');
 startButton.addEventListener('click', el => {
   database.functions.generateWorld(el);
   database.functions.generateSky(el);
-  database.functions.generateCloud(database.functions.randomNumber(3,5));
+  database.functions.generateCloud(database.functions.randomNumber(2,4));
+  database.functions.generateGrass();
+  database.functions.generateDirt();
+  database.functions.listeners();
 });
 
 const start = () => {
   database.functions.generateWorld();
   database.functions.generateSky();
-  database.functions.generateCloud(database.functions.randomNumber(3,5));
-  database.functions.generateCloud(database.functions.randomNumber(4,7));
-  database.functions.generateCloud(database.functions.randomNumber(3,6));
+  database.functions.generateCloud(database.functions.randomNumber(2,4));
+  database.functions.generateCloud(database.functions.randomNumber(1,3));
+  database.functions.generateDirt();
 }
 
 // Toolbar
 const tools = document.querySelectorAll('.tool');
 tools.forEach(tool => tool.addEventListener('click', database.functions.pickTool));
+const listen = () => {
+  document.querySelectorAll('.block').forEach(e => {
+  e.addEventListener('click', database.functions.mineBlock);
+});
+}
 // document.querySelector('.blocktest').addEventListener('click', database.functions.mineable);
